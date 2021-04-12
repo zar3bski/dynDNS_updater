1bd8adaf98fa204fbd9594f2b904e9de799d108d
zar3bski <zarebskidavid@gmail.com>
2021-04-13 00:31:02 +0200

    test : patching

--- tests/test_dyndns_updater.py
+++ tests/test_dyndns_updater.py
@@ -3,9 +3,15 @@ from unittest import TestCase
 from dyndns_updater.updater import GandiUpdater, Updater
 from unittest.mock import Mock, patch
 
-class GandiUpdater(TestCase): 
+class TestGandiUpdater(TestCase): 
 
-    # FIXME: error occurs with this constructor only during pytest
     @patch('dyndns_updater.updater.requests.post')
     def test_constructor_inheritance(self, mock_get): 
-        updater = GandiUpdater('https://dns.api.gandi.net/api/v5','some_key', 'somedomain.io', ['tower'])
+
+        normal_response = {"message": "Zone Created", "uuid": "ee788920-9bc5-11eb-823b-00163ea99cff"}
+
+        mock_get.return_value = Mock(ok=True)
+        mock_get.return_value.json.return_value = normal_response
+        
+        updater = GandiUpdater('https://dns.api.gandi.net','some_key', 'somedomain.io', ['tower'])
+        self.assertEqual(updater.zone_uuid, "ee788920-9bc5-11eb-823b-00163ea99cff")
