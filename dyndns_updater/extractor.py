f941650da38e56359a2e39b20cfb5b86a85b8e92
zar3bski <zarebskidavid@gmail.com>
2021-04-13 18:36:22 +0200

    feat : extractor makes sure there is only 1 match

--- dyndns_updater/extractor.py
+++ dyndns_updater/extractor.py
@@ -6,5 +6,9 @@ class Extractor:
     @classmethod
     def extract_first_field_value(cls, collection, filter_field, filter_value, field):
         """get value of the FIRST json record matching FILTER_FIELD in the given COLLECTION"""
-        filtered_record = cls._filter_records(collection, filter_field, filter_value)
-        return next(filtered_record).get(field)
+        filtered_record = list(cls._filter_records(collection, filter_field, filter_value))
+        if len(filtered_record)==1: 
+            return filtered_record[0].get(field)
+        else: 
+            raise ValueError("Multiple records matching FILTER_FIELD")
+            
--- tests/test_dyndns_updater.py
+++ tests/test_dyndns_updater.py
@@ -4,44 +4,59 @@ from dyndns_updater.updater import GandiUpdater, Updater
 from dyndns_updater.extractor import Extractor
 from unittest.mock import Mock, patch
 import json
+import pytest
 
 
-class TestExtractor(TestCase): 
-
-    def test_(self): 
+class TestExtractor(TestCase):
+    def test_(self):
         with open("./tests/out/response-2.json") as payload:
-            filtered = Extractor._filter_records(json.load(payload), "serial", 1609351415)
-            self.assertTrue(len(list(filtered)),1)
+            filtered = Extractor._filter_records(
+                json.load(payload), "serial", 1609351415
+            )
+            self.assertTrue(len(list(filtered)), 1)
 
-    def test_2(self): 
+    def test_2(self):
         with open("./tests/out/response-2.json") as payload:
-            filtered = Extractor.extract_first_field_value(json.load(payload), "serial", 1609351415, "expire")
+            filtered = Extractor.extract_first_field_value(
+                json.load(payload), "serial", 1609351415, "expire"
+            )
             self.assertTrue(type(filtered), json)
             self.assertEqual(filtered, 604800)
 
-class TestGandiUpdater(TestCase): 
-    
-    @patch('dyndns_updater.updater.requests.post')
-    def test_constructor_inheritance(self, mock_get): 
+    def test_multiplematch_raise_exception(self):
+        with pytest.raises(ValueError):
+            with open("./tests/out/multiple_match.json") as payload:
+                filtered = Extractor.extract_first_field_value(
+                    json.load(payload), "serial", 1609351415, "name"
+                )
 
-        normal_response = {"message": "Zone Created", "uuid": "ee788920-9bc5-11eb-823b-00163ea99cff"}
+
+class TestGandiUpdater(TestCase):
+    @patch("dyndns_updater.updater.requests.post")
+    def test_constructor_inheritance(self, mock_get):
+
+        normal_response = {
+            "message": "Zone Created",
+            "uuid": "ee788920-9bc5-11eb-823b-00163ea99cff",
+        }
 
         mock_get.return_value = Mock(ok=True)
         mock_get.return_value.json.return_value = normal_response
-        
-        #updater = GandiUpdater('https://dns.api.gandi.net','some_key', 'somedomain.io', ['tower'])
-        #self.assertEqual(updater.zone_uuid, "ee788920-9bc5-11eb-823b-00163ea99cff")
 
+        # updater = GandiUpdater('https://dns.api.gandi.net','some_key', 'somedomain.io', ['tower'])
+        # self.assertEqual(updater.zone_uuid, "ee788920-9bc5-11eb-823b-00163ea99cff")
 
-    @patch('dyndns_updater.updater.requests.get')
-    def test_uuid_retreival(self, mock_get): 
+    @patch("dyndns_updater.updater.requests.get")
+    def test_uuid_retreival(self, mock_get):
         with open("./tests/out/response-2.json") as payload:
-            
+
             normal_response = json.load(payload)
 
-            #mock_get.return_value = Mock(ok=True)
+            # mock_get.return_value = Mock(ok=True)
             mock_get.return_value.json.return_value = normal_response
 
-            updater = GandiUpdater('https://dns.api.gandi.net','some_key', 'somedomain.io', ['tower'])
+            updater = GandiUpdater(
+                "https://dns.api.gandi.net", "some_key", "somedomain.io", ["tower"]
+            )
 
             self.assertEqual(updater.zone_uuid, "00163ee24379-089b3cc4-5b57-11e8-b297")
